47fd19f7a5b7eb7a6bad78ea2c15a44ef30da8ea
unknown <168447636@qq.com>
2023-03-30 18:56:17 +0800

    update

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -47,22 +47,22 @@ def responseMsg():
             help += "010-<a href='https://mubu.com/doc/4pNs1XBo_f9'>安全扫描</a>\n"
             help += "011-<a href='https://mubu.com/doc/3Bxi3I9DCo'>Raspberry pi4使用记录</a>\n"
             help += "012-<a href='https://mubu.com/doc/1XiLnBztCo'>K8S启动盘使用帮助</a>\n"
-            help += "\n待完成waiting:\n"
-            help += "013-<a href=''>试用pandas分析数据</a>\n"
-            help += "014-<a href=''>性能优化</a>\n"
-            help += "015-<a href=''>Python实例</a>\n"
-            help += "016-<a href=''>日志收集系统</a>\n"
-            help += "017-<a href=''>来自编个监控系统</a>\n"
-            help += "018-<a href=''>从win10到centos7</a>\n"
+            # help += "\n待完成waiting:\n"
+            # help += "013-<a href=''>试用pandas分析数据</a>\n"
+            # help += "014-<a href=''>性能优化</a>\n"
+            # help += "015-<a href=''>Python实例</a>\n"
+            # help += "016-<a href=''>日志收集系统</a>\n"
+            # help += "017-<a href=''>来自编个监控系统</a>\n"
+            # help += "018-<a href=''>从win10到centos7</a>\n"
             help += "\nCloudMan 每天5分钟系列:\n"
-            # help += "<a href='https://mp.weixin.qq.com/s/7o8QxGydMTUe4Q7Tz46Diw'>[Docker教程]</a>\n"
-            # help += "<a href='https://mp.weixin.qq.com/s/RK6DDc8AUBklsUS7rssW2w'>[Kubernetes教程]</a>\n"
-            # help += "<a href='https://mp.weixin.qq.com/s/QtdMkt9giEEnvFTQzO9u7g'>[OpenStack教程]</a>\n"
-            # help += "\n其它:\n"
-            # help += "001-<a href='https://mubu.com/doc/3u65WbvQsp'>SimpleComputerWords</a>\n"
-            # help += "002-<a href='https://mubu.com/doc/3mtscGgyIo'>简述测试概念与工具</a>\n"
-            # help += "003-<a href='https://mubu.com/doc/LYdGMKtto'>IT架构图</a>\n"
-            # help += "004-<a href='https://mp.weixin.qq.com/mp/homepage?__biz=Mzg4MjAyMDgzMQ==&hid=1&sn=ce7139573c267c56ae45f026c4242045'>LinuxMan往期目录</a>\n"
+            help += "<a href='https://mp.weixin.qq.com/s/7o8QxGydMTUe4Q7Tz46Diw'>[Docker教程]</a>\n"
+            help += "<a href='https://mp.weixin.qq.com/s/RK6DDc8AUBklsUS7rssW2w'>[Kubernetes教程]</a>\n"
+            help += "<a href='https://mp.weixin.qq.com/s/QtdMkt9giEEnvFTQzO9u7g'>[OpenStack教程]</a>\n"
+            help += "\n其它:\n"
+            help += "001-<a href='https://mubu.com/doc/3u65WbvQsp'>SimpleComputerWords</a>\n"
+            help += "002-<a href='https://mubu.com/doc/3mtscGgyIo'>简述测试概念与工具</a>\n"
+            help += "003-<a href='https://mubu.com/doc/LYdGMKtto'>IT架构图</a>\n"
+            help += "004-<a href='https://mp.weixin.qq.com/mp/homepage?__biz=Mzg4MjAyMDgzMQ==&hid=1&sn=ce7139573c267c56ae45f026c4242045'>LinuxMan往期目录</a>\n"
 
             print(help)
             if params["MsgType"] == "text":
@@ -70,11 +70,11 @@ def responseMsg():
                 if keyword == "help":
                     content = help
                 else:
-                    content = "答1:<a href='https://jaywcjlove.gitee.io/linux-command/c/" + \
+                    content = "参考:\n1:<a href='https://jaywcjlove.gitee.io/linux-command/c/" + \
                         keyword + ".html'>" + keyword + "</a>\n"
-                    content += "Link2:<a href='https://www.linuxcool.com/" + \
+                    content += "2:<a href='https://www.linuxcool.com/" + \
                         keyword + "'>" + keyword + "</a>\n"
-                    content += "Link3:<a href='https://man.linuxde.net/" + \
+                    content += "3:<a href='https://man.linuxde.net/" + \
                         keyword + "'>" + keyword + "</a>"
                 retParams = {
                     "ToUserName": params["FromUserName"],
